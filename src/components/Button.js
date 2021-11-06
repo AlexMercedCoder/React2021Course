a915843ebcce6743bce4f368d2cf704e82a0a452
Alex Merced <alex@alexmerced.dev>
2021-11-06 10:20:03 -0400

    video 1

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+function Button(){
+    return <button style={{
+        backgroundColor: "blue",
+        padding: "10px",
+        color: "white",
+        fontSize:"1.5em",
+        fontWeight: "900"
+    }}>
+        Button
+        </button>
+}
+
+export default Button
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Example = <h1>Hello</h1>
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
